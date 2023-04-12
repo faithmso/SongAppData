e48e9a41e28096c423dd53b080491f02f27cc48c
FaithMso <>
2023-04-12 07:57:46 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,44 @@
+song_data = {'Retro Words': ['pop', 'warm', 'happy', 'electronic', 'synth'],
+             'Wait For Limit': ['rap', 'upbeat', 'romance', 'relationship'],
+             'Stomping Cue': ['country', 'fiddle', 'party'],
+             'Lowkey Space': ['electronic', 'dance', 'synth', 'upbeat'],
+             'Back To Art': ['pop', 'sad', 'emotional', 'relationship'],
+             'Blinding Era': ['rap', 'intense', 'moving', 'fast'],
+             'Down To Green Hills': ['country', 'relaxing', 'vocal', 'emotional'],
+             'Double Lights': ['electronic', 'chill', 'relaxing', 'piano', 'synth']}
+
+user_liked_song = {'Back To Art': ['pop', 'sad', 'emotional', 'relationship']}
+user_disliked_song = {'Retro Words': ['pop', 'warm', 'happy', 'electronic', 'synth']}
+
+# Write your code below!
+tag_diff = set(user_liked_song['Back To Art']).difference(set(user_disliked_song['Retro Words']))
+
+recommended_songs = {}
+for key, val in song_data.items():
+  for tag in val:
+    if tag in tag_diff:
+      if key not in user_liked_song and key not in user_disliked_song:
+        recommended_songs[key] = val
+
+print(recommended_songs)
+
+user_song_history = {'Retro Words': ['pop', 'warm', 'happy', 'electronic', 'synth'],
+                     'Stomping Cue': ['country', 'fiddle', 'party'],
+                     'Back To Art': ['pop', 'sad', 'emotional', 'relationship'],
+                     'Double Lights': ['electronic', 'chill', 'relaxing', 'piano', 'synth']}
+
+friend_song_history = {'Lowkey Space': ['electronic', 'dance', 'synth', 'upbeat'],
+                     'Blinding Era': ['rap', 'intense', 'moving', 'fast'],
+                     'Wait For Limit': ['rap', 'upbeat', 'romance', 'relationship'],
+                     'Double Lights': ['electronic', 'chill', 'relaxing', 'piano', 'synth']}
+
+# Write your code below!
+user_tags = set()
+for key, val in user_song_history.items():
+  user_tags.update(set(val))
+friend_tags = set()
+for key, val in friend_song_history.items():
+  friend_tags.update(set(val))
+
+unique_tags = user_tags.symmetric_difference(friend_tags)
+print(unique_tags)
